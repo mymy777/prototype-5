3ac88e16801229dc67cdae92eb8bbb5926b40337
Amaya Reed <amreed1@s.sfusd.edu>
2023-04-11 13:20:53 -0700

    5.2

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -12,6 +12,9 @@ public class Target : MonoBehaviour
     private float xRange = 4;
     private float ySpawnPos = -6;
 
+    public ParticleSystem explosionParticle;
+    public int pointValue;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -33,7 +36,8 @@ public class Target : MonoBehaviour
     private void OnMouseDown() 
     {
         Destroy(gameObject);
-        gameManager.UpdateScore(5);
+        Instantiate(explosionParticle, transform.position, explosionParticle.transform.rotation);
+        gameManager.UpdateScore(pointValue);
     }
     
     private void OnTriggerEnter(Collider other) 
