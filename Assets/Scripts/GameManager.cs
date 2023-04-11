b09ac9d1c96f1d369f57f96d8d230b34bcb32dbf
Amaya Reed <amreed1@s.sfusd.edu>
2023-04-11 14:30:55 -0700

    added redo button

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -2,19 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public bool isGameActive;
     private int score;
     private float spawnRate  = 1.0f;
    
     // Start is called before the first frame update
     void Start()
     {
-      StartCoroutine(SpawnTarget());  
+      isGameActive = true;
       score = 0;
+
+      StartCoroutine(SpawnTarget());  
       UpdateScore(0);
     }
 
@@ -26,7 +31,7 @@ public class GameManager : MonoBehaviour
 
     IEnumerator SpawnTarget()
     {
-        while(true) 
+        while(isGameActive) 
         {
             yield return new WaitForSeconds(spawnRate);
             int index = Random.Range(0, targets.Count);
@@ -38,5 +43,16 @@ public class GameManager : MonoBehaviour
     {
         score += scoreToAdd;
         scoreText.text = "Score:" + score;
+    }
+
+    public void GameOver() 
+    {
+         gameOverText.gameObject.SetActive(true);
+         isGameActive = false;
+    }
+
+    public void RestartGame() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
 }
